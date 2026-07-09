638188550678d053ee041715faea76d65ebc96d5
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-07-08 23:10:42 -0400

    chore: Incluído exemplo de teste de repositório no index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { TerminalController } from './controllers/TerminalController.js'
+import {buscarLivroPorTitulo, criaLivro, listarLivros} from "./repositories/LivroRepository";
 
 async function main() {
     const terminalController = new TerminalController();
@@ -7,11 +8,21 @@ async function main() {
 }
 
 main();
+// testes();
+/**
+ * Forma SIMPLES de testar os repositórios.
+ *
+ * OBS: Tem que comentar a linha 10 main() e des-comentar a linha 11 - testes()
+ * e o item abaixo a result... ser testado e também o console.log respectivo.
+ */
+async function testes() {
+    // const resultListar = await listarLivros()
+    // const resultBuscar = await buscarLivroPorTitulo("o amor")
+    // const resultCriar = await criaLivro("Teste Titulo 6", "12345e", 1953, 3 ,12)
+    // console.log(resultListar);
+    // console.log(resultBuscar);
+    // console.log(resultCriar);
 
-
-// testarConexao();
-
-// async function testarConexao() {
 //     try {
 //         const resultado = await pool.query('SELECT NOW();');
 //         console.log('🎉 Conexão com o banco realizada com sucesso!');
@@ -23,4 +34,4 @@ main();
 //         // Fecha o pool para o Node não ficar "preso" rodando no terminal
 //         await pool.end();
 //     }
-// }
+}
