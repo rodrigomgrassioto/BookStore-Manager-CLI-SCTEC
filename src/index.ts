13fe6b560051f5e44a8bb559770f73fd72eea1e5
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-07-09 21:06:55 -0300

    test: Testa o LivroService

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import { TerminalController } from './controllers/TerminalController.js'
-import {buscarLivroPorTitulo, criaLivro, listarLivros} from "./repositories/LivroRepository";
+import {
+    atualizarLivroRP,
+    buscarLivroPorTituloRP,
+    criaLivroRP,
+    deletarLivroRP,
+    listarLivrosRP
+} from "./repositories/LivroRepository";
+import {atualizarLivroServ, criarLivroServ, listarLivrosServ} from './services/LivroService'
 
 async function main() {
     const terminalController = new TerminalController();
@@ -7,8 +14,8 @@ async function main() {
     await terminalController.iniciarMenu();
 }
 
-main();
-// testes();
+// main();
+testes();
 /**
  * Forma SIMPLES de testar os repositórios.
  *
@@ -18,10 +25,20 @@ main();
 async function testes() {
     // const resultListar = await listarLivros()
     // const resultBuscar = await buscarLivroPorTitulo("o amor")
-    // const resultCriar = await criaLivro("Teste Titulo 6", "12345e", 1953, 3 ,12)
+    // const resultCriar = await criaLivro("Teste Titulo 6", "12345f", 1953, 3 ,12)
+    // const resultAtualizar = await atualizarLivro(3,"Teste Atualização 2", "12345e", 1953, 3 ,12)
+    // const resultDeletar = await deletarLivro(32);
+    // const listarLivrosComService = await listarLivrosServ()
+    // const criarLivroComService = await criarLivroServ('Livro com validador de isbn','978535912432', 5, 2, -350)
+    const atualizarLivroComService = await atualizarLivroServ(1,'Dom Casmurro','9788594318602', 1899, 5, 1)
     // console.log(resultListar);
     // console.log(resultBuscar);
     // console.log(resultCriar);
+    // console.log(resultAtualizar);
+    // console.log(resultDeletar);
+    // console.log(listarLivrosComService);
+    // console.log(criarLivroComService);
+    console.log(atualizarLivroComService);
 
 //     try {
 //         const resultado = await pool.query('SELECT NOW();');
