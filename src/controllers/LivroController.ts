5f6f6ad49eeec3c00887c3ee55b336fbc88c312f
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-07-11 13:50:35 -0300

    wip: Função livroControllerCriar em andamento

--- src/controllers/LivroController.ts
+++ src/controllers/LivroController.ts
@@ -9,15 +9,15 @@ export async function livroControllerCriar(): Promise<void> {
     // 1 Dados do usuário
     const titulo = await fazerPergunta("Título do livro: ");
     const isbn = await fazerPergunta("Código ISBN: ");
-    const qtdEstoqueStr = await fazerPergunta("Quantidade em estoque: ");
-    const idAutorStr = await fazerPergunta("ID do Autor: ");
-    const anoPublicacaoStr = await fazerPergunta("Ano de publicação (Opcional): ", {aceitarVazio: true});
+    const quantidade_estoque = await fazerPergunta("Quantidade em estoque: ", {tipoRetorno: 'i'});
+    const id_autor = await fazerPergunta("ID do Autor: ", {tipoRetorno: 'i'});
+    const ano_publicacao = await fazerPergunta("Ano de publicação (Opcional): ", {aceitarVazio: true, tipoRetorno: 'i'});
 
     // Converte string para number
-    const quantidade_estoque = parseInt(qtdEstoqueStr, 10);
-    const id_autor = parseInt(idAutorStr, 10);
-    const ano_publicacao = anoPublicacaoStr ? parseInt(anoPublicacaoStr, 10) : undefined;
 
+    console.log("===== RETORNOS ====")
+    console.log(titulo);
+    console.log(isbn);
     console.log(quantidade_estoque);
     console.log(id_autor);
     console.log(ano_publicacao);
