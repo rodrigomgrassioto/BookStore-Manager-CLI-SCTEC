61dd6a236ad778521626254b4ef60b13f97c2de6
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-07-10 21:58:43 -0300

    feat: Cria function deleta LivroService

--- src/services/LivroService.ts
+++ src/services/LivroService.ts
@@ -7,14 +7,16 @@ import {
     listarLivrosRP
 } from "../repositories/LivroRepository";
 import {validarISBN} from "../utils/validadores";
+import {livroJaFoiEmprestadoRP} from "../repositories/EmprestimoRepository";
 
 export async function listarLivrosServ(): Promise<LivroCompletoModel[]>  {
     return await listarLivrosRP();
 }
 
-export async function buscarLivroPorTituloServ(titulo:string){
+export async function buscarLivroPorTituloServ (titulo:string): Promise<LivroCompletoModel[]>{
     if (!titulo || !titulo.trim())
-        throw new Error("Necessário informar título.");
+        throw new Error("❌ Necessário informar título.");
+    return await buscarLivroPorTituloRP(titulo);
 }
 
 export async function criarLivroServ(
@@ -22,10 +24,10 @@ export async function criarLivroServ(
     id_autor: number, ano_publicacao?: number): Promise<LivroModel> {
     // Não permitir campos vazios
     if (!titulo || !titulo.trim() || !isbn || !isbn.trim() || !quantidade_estoque || !id_autor)
-        throw new Error("Os campos (título, isbn, quantidade em estoque, e id do Autor) são obrigatórios.");
+        throw new Error("❌ Os campos (título, isbn, quantidade em estoque, e id do Autor) são obrigatórios.");
 
     if(!validarISBN(isbn))
-        throw new Error("Código ISBN inválido.");
+        throw new Error("❌ Código ISBN inválido.");
 
     return await criaLivroRP(titulo, isbn, quantidade_estoque, id_autor, ano_publicacao);
 }
@@ -35,13 +37,19 @@ export async function atualizarLivroServ(
     quantidade_estoque:number, id_autor: number):Promise<LivroModel> {
 
     if (!id_livro || !titulo || !titulo.trim() || !isbn || !isbn.trim() || !quantidade_estoque || !id_autor)
-        throw new Error("Os campos (Id do livro, título, isbn, quantidade em estoque, e id do Autor) são obrigatórios.");
+        throw new Error("❌ Os campos (Id do livro, título, isbn, quantidade em estoque, e id do Autor) são obrigatórios.");
 
     if (!validarISBN(isbn))
-        throw new Error("Código ISBN inválido.");
+        throw new Error("❌ Código ISBN inválido.");
 
     const result = await atualizarLivroRP(id_livro, titulo, isbn, quantidade_estoque, id_autor, ano_publicacao);
     if (!result)
-        throw new Error("Erro ao atualizar livro.");
+        throw new Error("❌ Erro ao atualizar livro.");
     return result;
+}
+
+export async function deletarLivroServ(id: number):Promise<boolean> {
+    if(await livroJaFoiEmprestadoRP(id))
+        throw new Error("❌ Livro já foi emprestado, não pode ser excluído.", );
+    return await deletarLivroRP(id)
 }
