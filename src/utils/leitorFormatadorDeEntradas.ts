6be24ee6b0cea49733c3fb6f83472bec79a270df
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-07-17 19:04:12 -0300

    refactor: Atualiza import

--- src/utils/leitorFormatadorDeEntradas.ts
+++ src/utils/leitorFormatadorDeEntradas.ts
@@ -1,5 +1,5 @@
 import * as readline from 'readline';
-import {pergunta} from "./consoleLogCor";
+import {pergunta} from "./estilo";
 
 export const rl = readline.createInterface({
     input: process.stdin,
