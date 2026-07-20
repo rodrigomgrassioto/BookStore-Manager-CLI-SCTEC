a76efc11cafde7963dad301f875c3c51676ac4ee
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-07-20 11:59:49 -0300

    wip: Atender RF05 e RF19 em autor

--- src/repositories/AutorRepository.ts
+++ src/repositories/AutorRepository.ts
@@ -1,35 +1,57 @@
 import { AutorModel } from '../models/AutorModel';
+import { Pool } from "pg";
 import { pool } from '../database/connection';
 
-export async function cadastrarAutor(
-    nome: string, 
-    nacionalidade?: string
-): Promise<AutorModel> {
-    const sql = 
-        `INSERT INTO autores (nome, nacionalidade)
+export interface IAutorRepository {
+    cadastrar(nome: string, nacionalidade?: string): Promise<AutorModel>;
+    listar(): Promise<AutorModel[]>;
+    buscarPorId(id_autor: number): Promise<AutorModel | null>;
+    atualizar(id_autor: number, nome: string, nacionalidade?: string): Promise<AutorModel>;
+    deletar(id_autor: number): Promise<boolean>;
+}
+
+export class AutorRepository implements IAutorRepository {
+    private readonly conexao: Pool;
+
+    constructor(conexao: Pool = pool) {
+        this.conexao = conexao;
+    }
+
+    public async cadastrarAutor(nome: string, nacionalidade?: string): Promise<AutorModel> {
+        const sql =
+            `INSERT INTO autores (nome, nacionalidade)
          VALUES ($1, $2)
          RETURNING *`;
 
-    const result = await pool.query<AutorModel>(sql, [nome, nacionalidade ?? null]);
-    return result.rows[0];
-};
+        const result = await pool.query<AutorModel>(sql, [nome, nacionalidade ?? null]);
+        return result.rows[0];
+    }
 
-export async function listarAutores(): Promise<AutorModel[]> {
-    const sql = 
-    `SELECT * FROM autores`;
+    public async listarAutores(): Promise<AutorModel[]> {
+        const sql =
+            `SELECT * FROM autores`;
 
-    const result = await pool.query<AutorModel>(sql, []);
-    return result.rows || null;
-};
+        const result = await pool.query<AutorModel>(sql, []);
+        return result.rows || null;
+    }
 
-export async function buscarAutorPorId(id_autor: number): Promise<AutorModel | null> {
-    const sql = 
-    `SELECT * FROM autores 
+    export async function buscarAutorPorId(id_autor: number): Promise<AutorModel | null> {
+        const sql =
+            `SELECT * FROM autores 
      WHERE id_autor = $1`;
 
-    const result = await pool.query<AutorModel>(sql, [id_autor]);
-    return result.rows[0] || null;
-};
+        const result = await pool.query<AutorModel>(sql, [id_autor]);
+        return result.rows[0] || null;
+    }
+
+}
+
+
+
+
+
+
+
 
 export async function atualizarAutor(id_autor: number, nome: string, nacionalidade?: string): Promise<AutorModel> {
     const sql = 
